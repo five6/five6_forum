e07e4a29bdd43ed8ebdb71e65388e1c44a5eb9d6
five6 <936333511@qq.com>
2018-01-09 19:54:53 +0800

    添加qq 登录图标

--- app/service/common/files.js
+++ app/service/common/files.js
@@ -3,13 +3,12 @@
 const Service = require('egg').Service;
 const fs = require('fs');
 const path = require('path');
-const sizeOf = require('sizeof');
-const mime = require('mime-types');
+const size = require('image-size');
 const sendToWormhole = require('stream-wormhole');
 const toArray = require('stream-to-array');
 module.exports = app => {
   class FileService extends Service {
-    async files(ctx) {
+    async files() {
       const stream = await this.ctx.getFileStream();
       let buf;
       try {
@@ -20,22 +19,9 @@ module.exports = app => {
         throw err;
       }
       const filename = encodeURIComponent(stream.filename);
-      console.log(stream);
       const md5 = app.md5(filename + Date.now());
       const target = path.join(this.config.baseDir, 'app/public/files', md5);
       await fs.writeFile(target, buf);
-      const dimensions = sizeOf(stream);
-      console.log(dimensions);
-      const meta = {
-        size: stream.fields.size,
-        mime: stream.fields.mimetype,
-        name: stream.fields.name,
-        width: dimensions.width,
-        height: dimensions.height,
-      };
-      meta._id = md5;
-      const fileDoc = new ctx.model.files(meta);
-      await fileDoc.save();
       return md5;
     }
   }
