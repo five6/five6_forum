37c9bb44793a99809e413fb59591c7df688e8643
five6 <936333511@qq.com>
2018-02-05 15:37:00 +0800

    添加model

---
+++ app/model/forum.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const ForumSchema = new mongoose.Schema({
+    _id: { type: Object },
+    category: { type: String },
+    title: { type: String },
+    description: { type: String },
+    author_user: { type: String },
+    visit_count: { type: Number, default: 0 },
+    create_at: { type: Date, default: Date.now },
+  });
+  ForumSchema.index({ create_at: -1 });
+  ForumSchema.index({ author_user: 1, create_at: -1 });
+  return mongoose.model('Forum', ForumSchema);
+};
--- app/model/forum_topic.js
+++ app/model/forum_topic.js
@@ -4,22 +4,17 @@ module.exports = app => {
   const mongoose = app.mongoose;
   const TopicSchema = new mongoose.Schema({
     _id: { type: Object },
-    author_user: { type: String },
-    type: { type: String, default: 'share' },
+    forum_id: { type: String },
     title: { type: String },
     content: { type: String },
+    author_user: { type: String },
     top: { type: Boolean, default: false }, // 置顶
     good: { type: Boolean, default: false }, // 精华
-    lock: { type: Boolean, default: false }, // 系统不显示
-    reply_count: { type: Number, default: 0 },
+    if_stop_post: { type: Boolean, default: false }, // 是否截止回帖
     visit_count: { type: Number, default: 0 },
-    last_reply: { type: String },
-    last_reply_at: { type: Date, default: Date.now },
     create_at: { type: Date, default: Date.now },
-    update_at: { type: Date, default: Date.now },
   });
   TopicSchema.index({ create_at: -1 });
-  TopicSchema.index({ top: -1, last_reply_at: -1 });
   TopicSchema.index({ author_user: 1, create_at: -1 });
-  return mongoose.model('Topic', TopicSchema);
+  return mongoose.model('ForumTopic', TopicSchema);
 };
