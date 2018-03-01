08891a339763e51472852a25d35456d3a1aed8e9
five6 <936333511@qq.com>
2018-03-01 17:39:14 +0800

    修改file上传api返回结构为json，解决jquery.fileupload.js接收数据只能为json格式bug

--- app/public/js/user/profile.js
+++ app/public/js/user/profile.js
@@ -17,22 +17,28 @@ $(function () {
   $('#id_upload').fileupload({
     url: '/api/v1/files',
     dataType: 'json',
+    autoUpload: true,
     dropZone: null,
     pasteZone: null,
+    maxNumberOfFiles: 1,//最大上传文件数目
+    maxFileSize: 5000000,
     start(start) {
-      console.log(start);
+      // console.log(start);
     },
     done(e, data) {
-      const result = data.result[0];
-      console.log(result);
+      const md5 = data.result ? data.result.md5 : '';
+      console.log(md5);
     },
     fail(err) {
       console.log(err);
     },
     always: () => {
     },
-    progress: () => {
-      //   var progress = parseInt(data.loaded / data.total * 100, 10);
+    progress: (data) => {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      if (progress === 100) {
+        toastr.success('头像上传成功！', '通知');
+      }
       //   $('#progress-bar').css('width', progress + '%').text(progress + '%');
     },
   });
--- app/service/common/files.js
+++ app/service/common/files.js
@@ -4,24 +4,24 @@ const Service = require('egg').Service;
 const fs = require('fs');
 const path = require('path');
 const sendToWormhole = require('stream-wormhole');
-const toArray = require('stream-to-array');
+const awaitWriteStream = require('await-stream-ready').write;
 module.exports = app => {
   class FileService extends Service {
     async files() {
       const stream = await this.ctx.getFileStream();
-      let buf;
+      const filename = encodeURIComponent(stream.fields.name) + path.extname(stream.filename).toLowerCase();
+      const md5 = app.md5(filename + Date.now());
+      const target = path.join(this.config.baseDir, 'app/public/files', md5);
+      const writeStream = fs.createWriteStream(target);
       try {
-        const parts = await toArray(stream);
-        buf = Buffer.concat(parts);
+        await awaitWriteStream(stream.pipe(writeStream));
       } catch (err) {
         await sendToWormhole(stream);
         throw err;
       }
-      const filename = encodeURIComponent(stream.filename);
-      const md5 = app.md5(filename + Date.now());
-      const target = path.join(this.config.baseDir, 'app/public/files', md5);
-      await fs.writeFile(target, buf);
-      return md5;
+      console.log(md5);
+      // jquery-file-upload接受服务器端数据必须是json格式
+      return { md5: md5 };
     }
   }
   return FileService;
